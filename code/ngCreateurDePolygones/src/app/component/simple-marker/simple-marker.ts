c1f490934755d7c330b0606e7a5afc34cee81e95
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2026-02-02 16:08:33 -0500

    Ajout sélection marqueur

--- code/ngCreateurDePolygones/src/app/component/simple-marker/simple-marker.ts
+++ code/ngCreateurDePolygones/src/app/component/simple-marker/simple-marker.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { GoogleMap, MapAdvancedMarker, MapMarker } from '@angular/google-maps';
 
+// Interface pour un marqueur avec son style
+interface MarkerWithStyle {
+  position: google.maps.LatLngLiteral;
+  content: HTMLElement;
+}
+
 @Component({
   selector: 'app-simple-marker',
   imports: [GoogleMap, MapAdvancedMarker],
@@ -11,8 +17,20 @@ export class SimpleMarker {
     mapId: "MON_ID_DE_CARTE"
   };
 
-  // Marqueurs à afficher sur la carte
-  markerPositions: google.maps.LatLngLiteral[] = [];
+  // Icônes de marqueurs disponibles
+  availableIcons: string[] = [
+    '📍',
+    '💁‍♀️',
+    '💅',
+    '🫃',
+    '🫦',
+  ];
+
+  // Icône actuellement sélectionnée
+  selectedIcon: string = this.availableIcons[0];
+
+  // Marqueurs à afficher sur la carte (avec leur style)
+  markers: MarkerWithStyle[] = [];
 
   onClickOnMap(event: google.maps.MapMouseEvent) {
     console.log('Vous avez cliqué sur les coordonnées suivantes : ', '\n\tLat : ', event.latLng?.lat(), '\n\tLng : ', event.latLng?.lng());
@@ -20,6 +38,24 @@ export class SimpleMarker {
   }
 
   addMarker(event: google.maps.MapMouseEvent) {
-    this.markerPositions.push(event.latLng!.toJSON());
+    this.markers.push({
+      position: event.latLng!.toJSON(),
+      content: this.createMarkerContent(this.selectedIcon)
+    });
+  }
+
+  selectIcon(icon: string) {
+    this.selectedIcon = icon;
+  }
+
+  // Crée un élément HTML pour le marqueur personnalisé
+  createMarkerContent(icon: string): HTMLElement {
+    const div = document.createElement('div');
+    div.style.width = '30px';
+    div.style.height = '30px';
+    div.style.fontSize = '30px';
+    div.style.cursor = 'pointer';
+    div.innerText = icon;
+    return div;
   }
 }
